f273664d24bc29859e191e3d62050dd872aaf39f
belkarteur <25725699-belkarteur@users.noreply.replit.com>
2023-10-18 21:24:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+image_list = [rock, paper, scissors]
+computer = random.randint(0,2)
+print(computer)
+print(image_list[computer])
+
+user_input = input("what do you choose? type 0 for Rock, 1  for paper, 2 for scissors.\n")
+user = int(user_input)
+print(image_list[user])
+if computer == user:
+  print("tie") 
+if computer == 0 and user == 1:
+  print("you win")
+if computer == 0 and user == 2:
+  print("you lose")
+if computer == 1 and user == 0:
+  print("you lose")
+if computer == 1 and user == 2:
+  print("you win")
+if computer == 2 and user == 0:
+  print("you win")
+if computer == 2 and user == 1:
+  print("you lose")
+
+# game_images = [rock, paper, scissors]
+
+# user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+# if user_choice >= 3 or user_choice < 0: 
+#   print("You typed an invalid number, you lose!") 
+# else:
+#   print(game_images[user_choice])
+
+#   computer_choice = random.randint(0, 2)
+#   print("Computer chose:")
+#   print(game_images[computer_choice])
+
+#   if user_choice == 0 and computer_choice == 2:
+#     print("You win!")
+#   elif computer_choice == 0 and user_choice == 2:
+#     print("You lose")
+#   elif computer_choice > user_choice:
+#     print("You lose")
+#   elif user_choice > computer_choice:
+#     print("You win!")
+#   elif computer_choice == user_choice:
+#     print("It's a draw")
